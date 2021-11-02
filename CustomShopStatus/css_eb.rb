62de08052c4eee9084cda382271dce43f33b3b00
CACAO <76836489+cacao-soft@users.noreply.github.com>
2021-11-02 20:44:20 +0900

    v21.11.02 カスタムショップステータス [EB]
    タイプ1-3 を取り込みオプションで選択するようにした。

--- CustomShopStatus/css_eb.rb
+++ CustomShopStatus/css_eb.rb
@@ -1,6 +1,10 @@
 #
-# カスタムショップステータス [EB] (12.05.16)
+# カスタムショップステータス [EB] (21.11.02)
 #
+
+# 表示タイプ (1-3)
+CAO::ShopStatus::EBType = 1
+
 class Window_ShopStatus
   #--------------------------------------------------------------------------
   # ● ページ切り替えの処理が必要か
@@ -46,5 +50,68 @@ class Window_ShopStatus
       draw_actor_parameter_change(x, y + WLH * i, old_param, new_param)
     end
     @last_y += WLH * PARAMS.size + 8
+  end
+end
+
+class Window_ShopStatus
+  #--------------------------------------------------------------------------
+  # ● アクターの現装備と能力値変化の描画
+  #--------------------------------------------------------------------------
+  case
+  when 1
+    def draw_actor_parameter_change(x, y, old_param, new_param)
+      self.contents.font.color = normal_color
+      self.contents.draw_text(x + 80, y, 48, WLH, old_param, 2)
+      self.contents.font.color = system_color
+      self.contents.draw_text(x + 128, y, 32, WLH, VOCAB_SEPARATOR, 1)
+      if new_param
+        change_parameter_color(new_param <=> old_param)
+        self.contents.draw_text(x + 160, y, 48, WLH, new_param, 2)
+      else
+        self.contents.font.color = normal_color
+        self.contents.draw_text(x + 160, y, 48, WLH, VOCAB_NONE, 2)
+      end
+    end
+  when 2
+    def draw_actor_parameter_change(x, y, old_param, new_param)
+      self.contents.font.color = normal_color
+      self.contents.draw_text(x + 80, y, 40, WLH, old_param, 2)
+      self.contents.font.color = system_color
+      self.contents.draw_text(x + 120, y, 24, WLH, VOCAB_SEPARATOR, 1)
+      if new_param
+        value = new_param <=> old_param
+        change_parameter_color(value)
+        text_power = VOCAB_POWER[value]
+        self.contents.draw_text(x + 144, y, 40, WLH, new_param, 2)
+        self.contents.draw_text(x + 184, y, 24, WLH, text_power, 1)
+      else
+        self.contents.font.color = normal_color
+        self.contents.draw_text(x + 144, y, 40, WLH, VOCAB_NONE, 2)
+      end
+    end
+  when 3
+    def draw_actor_parameter_change(x, y, old_param, new_param)
+      if new_param
+        value = new_param <=> old_param
+        change_parameter_color(value)
+        text_power = VOCAB_POWER[value]
+        self.contents.draw_text(x + 80, y, 48, WLH, new_param, 2)
+        self.contents.draw_text(x + 128, y, 32, WLH, text_power, 1)
+        self.contents.font.color = normal_color
+        self.contents.draw_text(x + 160, y, 48, WLH, old_param, 2)
+      else
+        text_power = ""
+        self.contents.font.color = normal_color
+        self.contents.draw_text(x + 80, y, 48, WLH, VOCAB_NONE, 2)
+        self.contents.draw_text(x + 160, y, 48, WLH, VOCAB_NONE, 2)
+      end
+      if text_power.empty?
+        self.contents.font.color = system_color
+        self.contents.draw_text(x + 128, y, 32, WLH, VOCAB_SEPARATOR, 1)
+      end
+    end
+  else
+    def draw_actor_parameter_change(x, y, old_param, new_param)
+    end 
   end
 end
