2fac8934f8f7c2768a84bc51ce0a1a47652891dc
CACAO <76836489+cacao-soft@users.noreply.github.com>
2024-09-05 18:14:38 +0900

    v1.0.2 ディレクトリ操作

--- Dir.rb
+++ Dir.rb
@@ -3,7 +3,7 @@
 # ---------------------------------------------------------------------------
 #  Copyright (c) 2021 CACAO
 #  Released under the MIT License.
-#  https://opensource.org/licenses/mit-license.php
+#  https://opensource.org/license/mit
 # ---------------------------------------------------------------------------
 #  [Twitter] https://twitter.com/cacao_soft/
 #  [GitHub]  https://github.com/cacao-soft/
@@ -62,7 +62,7 @@ class DirEx
   # ● 定数
   #--------------------------------------------------------------------------
   MAX_PATH = 260              # パスの最大サイズ
-  PACK_CODE = "Z#{MAX_PATH}"  # 
+  PACK_CODE = "Z#{MAX_PATH}"  #
   #--------------------------------------------------------------------------
   # ● CSIDL 値
   #--------------------------------------------------------------------------
