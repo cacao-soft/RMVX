796dd16e26946c1cb84c775082192b268069a0d8
CACAO <76836489+cacao-soft@users.noreply.github.com>
2024-09-05 18:14:40 +0900

    v1.0.3 天候の設定

--- ExWeather.rb
+++ ExWeather.rb
@@ -3,7 +3,7 @@
 # ---------------------------------------------------------------------------
 #  Copyright (c) 2021 CACAO
 #  Released under the MIT License.
-#  https://opensource.org/licenses/mit-license.php
+#  https://opensource.org/license/mit
 # ---------------------------------------------------------------------------
 #  [Twitter] https://twitter.com/cacao_soft/
 #  [GitHub]  https://github.com/cacao-soft/
